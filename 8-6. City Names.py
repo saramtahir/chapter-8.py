6690a4bfc2e1ef2bee353e0dc05eb3ecff370798
saramtahir <saramtahir33@gmail.com>
2025-05-21 01:07:44 +0500

    Initial commit

---
+++ 8-6. City Names.py
@@ -0,0 +1,7 @@
+def city_country(city, country):
+    return f"{city}, {country}"
+
+# Call the function with three city-country pairs
+print(city_country("Santiago", "Chile"))
+print(city_country("Tokyo", "Japan"))
+print(city_country("Cairo", "Egypt"))
